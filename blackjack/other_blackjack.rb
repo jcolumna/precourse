5a0d3349a91ccc845a2c77421a64362339769f97
Joshua Columna <josh.columna@gmail.com>
2014-11-28 14:01:21 -0700

    Finished Blackjack - made it replayable, as well as ask the player's name.

--- blackjack/other_blackjack.rb
+++ blackjack/other_blackjack.rb
@@ -1,20 +1,13 @@
 #other_blackjack.rb
 #This is the same as other_blackjack.rb only it has the hand draw function...
 #Joshua Columna
-#10/21/14
+#11/28/14
 
 #require 'pry'
 
-#Allow the player to place a bet on a the next hand before any cards are dealt.
 #Deal two cards to player and dealer, leaving dealer's second card face down.
-#Options: allow player to split if their two cards are of the same value.
-# no doubling down or splitting is allowed on the already split hands.
-#Options: Allow the player to double down, doubling their bet if their total is 10 or 11, 
-# and taking only one more hit afterwards.
-#Options: If the dealer's face card is an ace, allow the player to buy insurance at a rate of half their bet.
-#If the dealer's hole card is a ten when their face card is an ace, house wins unless it was a push.
-# If insurance was bought and house did have blackjack, player keeps their chips. Otherwise, the extra half bet is lost.
-#Either way, game over.
+#If the player has blackjack, flip the dealer's hole card and compare.
+#If house has blackjack too, it's a push, otherwise, player wins. Game over.
 #Ask the player if he wants to hit until he decides not to.
 #If the player busts, game over.
 #Once the player decides to stay, flip the dealer's hole card. 
@@ -24,9 +17,6 @@
 #Game over.
 #If neither is higher, it's a push.
 #Game over.
-
-
-
 
 def calculate_total(hand) #[['2', 's'], ['a', 'd'], ... etc.] 
   
@@ -53,15 +43,9 @@ def calculate_total(hand) #[['2', 's'], ['a', 'd'], ... etc.]
   total
 end
 
-
-# def draw_board(pcards, dcards)
-# end
-
 def draw_hand(hand, who = 'Dealer', display_last = false)
   #Count the number of cards in a hand and display each part that many times on one line.
   #If !display_last, don't print the last card.
-    #If you're gonna use an iterator, just use it for everything.
-    #hand[c][val]
 
   c = 0
   last = []
@@ -71,7 +55,9 @@ def draw_hand(hand, who = 'Dealer', display_last = false)
   end
   hand.each {c +=1}
   spaces = c * 11 / 2 - 6 
-  #crappy formatting method attempts to center align the text
+  #crappy formatting method attempts to center align the hand's header.
+  #Works fine if the name is 6 chars long, ie. 'Player' or 'Dealer'
+  #But starts to get askew the longer the name is.
   spaces.times do 
     print " "
   end
@@ -110,102 +96,123 @@ def draw_hand(hand, who = 'Dealer', display_last = false)
   puts ""  
 end
 
-#Possible to include "Play Again?" logic here?
-def draw_board(mycards, dealercards, prompt, msg, dealers_turn = false)
+def draw_board(mycards, dealercards, prompt, msg, dealers_turn = false, needs_input = true)
   system 'cls' or system 'clear'
   puts "#{prompt}"
   puts ""
-  draw_hand(mycards, 'Player', true)
+  draw_hand(mycards, NAME, true)
   puts ""
   draw_hand(dealercards, 'Dealer', dealers_turn)
   puts ""
   puts msg
-  if !dealers_turn
+  if !dealers_turn && needs_input
     input = gets.chomp
   end
 end
 
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ["\u2665", "\u2666", "\u2663", "\u2660"] #Hearts, Diamonds, Clubs, Spades
-
-
-deck = values.product(suits)
-
-deck = deck.shuffle!
-
-mycards = []
-dealercards = []
-
-#initial deal
-
-2.times do
-  mycards << deck.pop
-  dealercards << deck.pop
-end
-
-#print out the initial hands
-#puts "You have #{mycards}, for a total of #{calculate_total(mycards)}."
-#puts "Dealer has #{dealercards}, for a total of #{calculate_total(dealercards)}."
-
-#FIRST: Test to see if player has BLACKJACK
-#Next: If it isn't BLACKJACK, ask if they want to hit or stay.
-#Then: Keep asking until player either stays, or their hand value is greater than 21.
-#Following that: If the player has 21, They win!
-#Unfortunately: If the player has more than 21, they lose.
-#Lastly: If the player chose to stay, play passes to dealer.
-game_over = false
-if calculate_total(mycards) == 21
-  draw_board(mycards, dealercards, "Player has Blackjack!", "Congratulations! You win!") 
-  game_over = true
-end
-if calculate_total(mycards) < 21 && !game_over
-  begin
-    input = draw_board(mycards, dealercards, "What would you like to do?", "(H to hit, S to stay.)")
-    if input.casecmp('h') == 0 && calculate_total(mycards) < 21
-      mycards << deck.pop
-    end   
-  end until input.casecmp('h') != 0 || calculate_total(mycards) >= 21
-end
-if calculate_total(mycards) == 21 && !game_over
-  draw_board(mycards, dealercards, "Player has 21!!", "Congrats! You win!")
-  game_over = true
-elsif calculate_total(mycards) > 21
-  draw_board(mycards, dealercards, "You bust!", "Better luck next time...")
-  game_over = true
-end
-# Next, play passes to dealer...
-if !game_over
-  draw_board(mycards, dealercards, "The dealer reveals their hole card...", "", true)
-  sleep (5)
-  if calculate_total(dealercards) < 17
-    begin 
-      dealercards << deck.pop
-      draw_board(mycards, dealercards, "The dealer draws a card.", "", true)
-      sleep (5)
-    end until calculate_total(dealercards) >= 17
+def blackjack_check(pl_cards, dl_cards) 
+  draw_board(pl_cards, dl_cards, "#{NAME} has Blackjack!", "Dealer has...", false, false)
+  sleep (3)
+  if calculate_total(dl_cards) == 21
+    draw_board(pl_cards, dl_cards, "Dealer also has Blackjack...", "No winners this time~", true)
+  else
+    draw_board(pl_cards, dl_cards, "Dealer only has #{calculate_total(dl_cards)} total!", "Congrats, #{NAME} wins!", true) 
   end
 end
-# The totals aren't going to change any further so we can just store the values instead
-# of calculating them all the time.
-dealer_total = calculate_total(dealercards)
-player_total = calculate_total(mycards)
-if dealer_total == 21 && !game_over
-  draw_board(mycards, dealercards, "Dealer has Blackjack!", "Oh no!! You lose.. Try again?", true)
-  game_over = true
-elsif dealer_total > 21 && !game_over
-  draw_board(mycards, dealercards, "The house is bust!", "Congratulations, you win!", true)
-  game_over = true
-elsif dealer_total == player_total && !game_over
-  draw_board(mycards, dealercards, "It's a push~", "No winners this time...", true)
-  game_over = true
-elsif dealer_total > player_total && !game_over
-  draw_board(mycards, dealercards, "Dealer wins!", "Better luck next time...", true)
-  game_over = true
-elsif dealer_total < player_total  && !game_over
-  draw_board(mycards, dealercards, "You win, Player!", "Congratulations!!", true)  
-  game_over = true
-end
 
+puts ""
+puts "Welcome to Blackjack!"
+puts ""
+puts "May I have your name?"
+NAME = gets.chomp
+puts "Thanks for joining us, #{NAME}!"
+sleep (2)
+puts "Let's begin, shall we?"
+sleep (1)
+
+begin
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  suits = ["\u2665", "\u2666", "\u2663", "\u2660"] #Hearts, Diamonds, Clubs, Spades
+
+  deck = values.product(suits)
+
+  deck = deck.shuffle!
+
+  mycards = []
+  dealercards = []
+
+  #initial deal
+   #mycards << ['J', "\u2665"] #---- These lines are for testing blackjack
+   #mycards << ['A', "\u2665"]
+   #dealercards << ['J', "\u2665"]
+   #dealercards << ['A', "\u2665"]
+  
+  2.times do
+    mycards << deck.pop
+    dealercards << deck.pop
+  end
+
+  game_over = false
+  if calculate_total(mycards) == 21 #Player wins unless dealer also has blackjack
+    blackjack_check(mycards, dealercards)
+    game_over = true
+  end
+  if calculate_total(mycards) < 21 && !game_over
+    begin
+      input = draw_board(mycards, dealercards, "What would you like to do?", "(H to hit, S to stay.)")
+      if input.casecmp('h') == 0 && calculate_total(mycards) < 21
+        mycards << deck.pop
+      end   
+    end until input.casecmp('h') != 0 || calculate_total(mycards) >= 21
+  end
+  if calculate_total(mycards) == 21 && !game_over
+    blackjack_check(mycards, dealercards)
+    game_over = true
+  elsif calculate_total(mycards) > 21
+    draw_board(mycards, dealercards, "#{NAME} busts!", "Better luck next time, #{NAME}...", false, false)
+    game_over = true
+  end
+  # Next, play passes to dealer...
+  if !game_over
+    draw_board(mycards, dealercards, "The dealer reveals their hole card...", "", true)
+    sleep (3)
+    if calculate_total(dealercards) < 17
+      begin 
+        dealercards << deck.pop
+        draw_board(mycards, dealercards, "The dealer draws a card.", "", true)
+        sleep (2)
+      end until calculate_total(dealercards) >= 17
+    end
+  end
+  # The totals aren't going to change any further so we can just store the values instead
+  # of calculating them all the time.
+  dealer_total = calculate_total(dealercards)
+  player_total = calculate_total(mycards)
+  if dealer_total == 21 && !game_over
+    draw_board(mycards, dealercards, "Dealer has Blackjack!", "Oh no!! #{NAME} loses.. ", true)
+    game_over = true
+  elsif dealer_total > 21 && !game_over
+    draw_board(mycards, dealercards, "The house is bust!", "Congratulations, #{NAME} wins!", true)
+    game_over = true
+  elsif dealer_total == player_total && !game_over
+    draw_board(mycards, dealercards, "It's a push~", "No winners this time...", true)
+    game_over = true
+  elsif dealer_total > player_total && !game_over
+    draw_board(mycards, dealercards, "Dealer wins!", "Better luck next time, #{NAME}...", true)
+    game_over = true
+  elsif dealer_total < player_total  && !game_over
+    draw_board(mycards, dealercards, "You win, #{NAME}!", "Congratulations!!", true)  
+    game_over = true
+  end
+  puts ""
+  puts "Play again? (y/n)"
+  input = gets.chomp
+  if input.casecmp('y') == 0
+    game_over = false
+  else
+    game_over = true
+  end    
+end until game_over
 
 
 
